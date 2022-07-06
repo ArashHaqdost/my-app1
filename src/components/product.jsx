d3105406999d904f1d8362d92d3ade08bde98045
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-07-06 17:14:23 +0430

    added the  new file

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,40 +1,52 @@
 import { Component } from "react";
 import '../product.css';
 
-
-class List extends Component{
-    state={
-        count: 0,
-        productName:"Laptop"
+const Product = ({title})=>{
+    const titleHandler = ()=>{
+        console.log((title))
     }
-
-    render(){
-        return(<>
-            <span>{this.state.productName}</span>
-            <span style={{margin: "10px"}}>{this.format()}</span>
-            <button onClick={this.increse}>+</button>
-            <button onClick={this.decrese}>-</button>
-            <button onClick={this.delete}>delete</button>
-        </>)
-    }
-    format(){
-        if(this.state.count ===0){
-            return 'Zero';
-        }else{
-            return this.state.count;
-        }
-    }
-    increse = ()=>{
-        const {count} = this.state;
-        this.setState({count: count + 1})
-    }
-    decrese = ()=>{
-        const {count}= this.state;
-        this.setState({count: count -1})
-    }
-    delete = ()=>{
-        return "delete";
-    }
+    return(
+    <div className="container">
+        <div className="product">{title}</div>
+    <div>
+        <button className="btn" onClick={titleHandler}>Titel</button>
+    </div>
+    </div>
+    )
 }
 
-export default List;
+export default Product
+// class List extends Component{
+//     state = {
+//         count: this.props.count,
+//     }
+//     render(){
+//         return(<div>
+//             <span>{this.props.productName}</span>
+//             <span style={{margin: "10px"}}>{this.format()}</span>
+//             <button onClick={this.increse}>+</button>
+//             <button onClick={this.decrese}>-</button>
+//             <button onClick={this.delete}>delete</button>
+//         </div>)
+//     }
+//     format(){
+//         if(this.state.count ===0){
+//             return 'Zero';
+//         }else{
+//             return this.state.count;
+//         }
+//     }
+//     increse = ()=>{
+//         const {count} = this.state;
+//         this.setState({count: count + 1})
+//     }
+//     decrese = ()=>{
+//         const {count}= this.state;
+//         this.setState({count: count -1})
+//     }
+//     delete = ()=>{
+//         return "delete";
+//     }
+// }
+
+// export default List;
