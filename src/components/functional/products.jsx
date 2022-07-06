93ff96b8f32e3ec6e54cfa38704c4dcb067b8218
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-07-06 17:15:25 +0430

    complete  the  delete state

---
+++ src/components/functional/products.jsx
@@ -0,0 +1,11 @@
+import Product from "./product";
+
+ const Products = ()=>{
+    return(<>
+    <Product  productName="iphone"/>
+    <Product  productName="Metacerw"/>
+    <Product  productName="catoshi"/>
+    </>);
+}
+
+export default Products;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import App  from './app';
+// import App  from './app';
 import ReactDOM  from 'react-dom';
 import  './product.css';
 // import 'bootstrap/dist/css/bootstrap.main.css';
-import Product from './components/product';
+// import Product from './components/functional/product';
+// import Products from "./components/functional/products"
+// import Product from './components/products';
+import App from './App';
 
-ReactDOM.render(<Product/>, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
