b1aacfcea6dc92535f22a5b89dbced2915ef8d39
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-07-14 11:08:54 +0430

    create app.js

--- src/app.js
+++ src/app.js
@@ -1,73 +1,15 @@
-// import Product from "./components/product"; 
-import React from "react";
-import ProductList from "./components/productList/ProductList";
- import { useState } from 'react'
+import { Component } from "react";
+// class App extends Component{
+//     render(){
+//         return  <h1>hellow React</h1>
+//     }
+// }
 
 function App(){
-    const [products, setProducts] = useState([
-            {id:1, Name:'Computer'},
-            {id:2, Name:'Car'},
-            {id:3, Name:'Phone'},
-            {id:4, Name:'HOuse'},
-        ]);
- const deleteProducts = (id)=>{
-    setProducts(products.filter((item)=> item.id !== id))
-
-}    
-    // const products =;
-    return(
-        <div className="container">
-     <ProductList  products={products} onDelete={deleteProducts} />
-        </div>
-    )
-
+    return <>
+    <h1 className="p-2 text-danger">hello world</h1>
+    <p>this its isthi wusoadjlk
+    </p>
+    </>
 }
-export default App
-
-
-// export default function MyApp(){
-//     return(
-//         <div>
-//             <h1>wellocme to my App</h1>
-//             <MyBatton/>
-//         </div>
-//     )
-// }
-// function MyBatton(){
-//     return(<button>my Button</button>)
-// }
-
-
-
-
-// import {Component} from "react";
-
-// //Calss Based Component
-// class App extends Component{
-//     //render() return an react element or jsx
-//     render(){
-//         return(
-//         <><h1>hello my world</h1> <h2>this is the react world</h2></>
-
-//         ) 
-
-//     }
-//  function  MyBtton(params) {
-    
-//  }
-// }
-// export default App;
-
-//Function based Component
-
-// function Listt(){
-//     return(
-//         <ul>
-//             <li>a</li>
-//             <li>b</li>
-//             <li>c</li>
-//             <li>d</li>
-//         </ul>
-//     );
-// }
-// export default (Listt);
+export default App;
