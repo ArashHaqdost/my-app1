3b7ededc6ba4928b71d43f76daa810ddb04327b6
Arash Haqdost <shokrullahhaqdost87@gmail.com>
2022-07-19 21:47:23 +0430

    added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import App from './app'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Prodcut from './Component/Product'
-// const element = <h1>hello world</h1>;
 
-ReactDOM.render(<Prodcut /> , document.getElementById('root'));
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById('root'));
